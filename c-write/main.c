cad5e480d684908450b674fbf3fded178f6a82bd
Jabelic <hibiki990523@gmail.com>
2021-11-20 17:42:44 +0900

    ADD: 入力を即座に出力して返すだけのプログラム

--- c-write/main.c
+++ c-write/main.c
@@ -1,22 +1,16 @@
 #include <sys/types.h>
-#include <unistd.h>
+#include <unistd.h>   // read, write
+#include <sys/uio.h>  // read, writev
+#include <inttypes.h> // u_int8_t
+#include <fcntl.h>    // open
 
-// ssize_t
-// write(int fd, const void *buf, size_t nbytes);
-
-// #include <sys/uio.h>
-
-// ssize_t
-// writev(int fd, const struct iovec *iov, int iovcnt);
-
-// struct iovec
-// {
-//     void *iov_base; /* Base address. */
-//     size_t iov_len; /* Length. */
-// };
-
+/** 入力を即座に出力して返すだけのプログラム */
 int main(int argc, char **argv)
 {
-    u_int8_t buf[4096];
+    u_int8_t buf[4096]; // 4096byte. ここに書き込む.
+    int fd = open(argv[1], O_RDONLY);
+    ssize_t nread;
+    while ((nread = read(0, buf, sizeof buf)) > 0)
+        write(1, buf, nread);
     return 0;
 }
